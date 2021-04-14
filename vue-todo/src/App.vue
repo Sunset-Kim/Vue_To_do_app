76d84b7c2d1ae1fd455799d32fd5caafc31b61f2
Sunset-Kim <wood2002@naver.com>
2021-04-14 22:22:26 +0900

    🎉 Project start Todo list for vue.js

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoList from './components/TodoList.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+ 
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoList': TodoList,
+    'TodoInput': TodoInput,
+    'TodoFooter': TodoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
