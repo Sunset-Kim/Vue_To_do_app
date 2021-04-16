a7fcc5204db12712eef172309dde6676fca8c5ac
Sunset-Kim <wood2002@naver.com>
2021-04-16 20:00:09 +0900

    🎨 implement components footer

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-      footer
+  <div class="clearAllContainer">
+    <span v-on:click="clearAll" class="clearAllBtn">Clear All</span>
+    
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearAll: function() {
+      console.log('안녕');
+      localStorage.clear();
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  .clearAllContainer{
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background: white;
+    border-radius: 5px;
+    margin: 0 auto;
+    cursor: pointer;
 
+  }
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
 </style>
